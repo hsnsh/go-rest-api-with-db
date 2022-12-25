d924e7720d25132525e5d3595649cae01bb66d6a
Hasan H. SAHIN <hsnsh@outlook.com>
2022-12-25 22:34:05 +0300

    code refactor

--- internal/helpers/response.go
+++ internal/helpers/response.go
@@ -17,25 +17,27 @@ type ResponseDto struct {
 func RespondErrorWithMessage(w http.ResponseWriter, message string) {
 	RespondErrorWithMessages(w, strings.Split(message, "\n"))
 }
-
 func RespondErrorWithMessages(w http.ResponseWriter, messages []string) {
 	respondBaseWithData(w, http.StatusBadRequest, ResponseDto{Success: false, Messages: messages})
 }
-
-func RespondOkWithData(w http.ResponseWriter, payload interface{}) {
-	respondBaseWithData(w, http.StatusOK, ResponseDto{Success: true, Data: payload})
-}
-
-func RespondCreatedWithData(w http.ResponseWriter, payload interface{}) {
-	respondBaseWithData(w, http.StatusCreated, ResponseDto{Success: true, Data: payload})
-}
-
 func RespondOk(w http.ResponseWriter) {
 	RespondWithCode(w, http.StatusOK)
 }
-
+func RespondOkWithData(w http.ResponseWriter, payload interface{}) {
+	respondBaseWithData(w, http.StatusOK, ResponseDto{Success: true, Data: payload})
+}
+func RespondCreatedWithData(w http.ResponseWriter, payload interface{}) {
+	respondBaseWithData(w, http.StatusCreated, ResponseDto{Success: true, Data: payload})
+}
 func RespondWithCode(w http.ResponseWriter, code int) {
 	w.WriteHeader(code)
+}
+
+func HandlePanicAndRecovery(w http.ResponseWriter, appLogger logger.IFileLogger) {
+	if r := recover(); r != nil {
+		appLogger.Warning(fmt.Sprintf("%s", r))
+		RespondWithCode(w, http.StatusInternalServerError)
+	}
 }
 
 func respondBaseWithData(w http.ResponseWriter, code int, apiResponse ResponseDto) {
@@ -50,11 +52,4 @@ func respondBaseWithData(w http.ResponseWriter, code int, apiResponse ResponseDt
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(response)
-}
-
-func HandlePanicAndRecovery(w http.ResponseWriter, appLogger logger.IFileLogger) {
-	if r := recover(); r != nil {
-		appLogger.Warning(fmt.Sprintf("%s", r))
-		RespondWithCode(w, http.StatusInternalServerError)
-	}
 }
